629c343257add9c91dd21b2c5ea70eeedbf21247
Tony Spiro <tspiro@tonyspiro.com>
2023-02-06 11:23:23 -0800

    edit: rm twitter

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link"
 import { siteConfig } from "@/config/site"
 
-import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
-import { buttonVariants } from "@/components/ui/button"
 
 export function SiteHeader() {
   return (
@@ -13,22 +11,6 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                  className: "text-slate-700 dark:text-slate-400",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
             <ThemeToggle />
           </nav>
         </div>
