5c28090c7f4d2e2f3da85f954032f521f67a8817
Tony Spiro <tspiro@tonyspiro.com>
2023-02-06 11:33:41 -0800

    edit: rm sticky header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,7 +6,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
+    <header className="top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
