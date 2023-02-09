ae5bad1eb7b3cf5fdbd0b93e910d8807f6b0cc4a
Tony Spiro <tspiro@tonyspiro.com>
2023-02-09 14:27:41 -0800

    add: github link

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link"
 import { siteConfig } from "@/config/site"
-
+import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { buttonVariants } from "@/components/ui/button"
 
 export function SiteHeader() {
   return (
@@ -11,6 +12,22 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                  className: "text-slate-700 dark:text-slate-400",
+                })}
+              >
+                <Icons.gitHub className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
             <ThemeToggle />
           </nav>
         </div>
