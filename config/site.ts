77a19adb3b001872e6942db6f623a1565602b17c
Tony Spiro <tspiro@tonyspiro.com>
2023-02-08 19:17:02 -0800

    edit: site name

--- config/site.ts
+++ config/site.ts
@@ -12,7 +12,7 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-  name: "Cosmic AI Assistant",
+  name: "Cosmic",
   description:
     "An AI writing assistant",
   mainNav: [],
